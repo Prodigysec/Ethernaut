69cebfdfa445f5fed4610ca8ad1ac47177e84808
Ian Raburu <74436110+Prodigysec@users.noreply.github.com>
2023-05-03 11:07:59 +0300

    Update Telephone.sol

--- 4-Telephone/Contracts/Telephone.sol
+++ 4-Telephone/Contracts/Telephone.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;  //floating point introduces compiler error
 
+contract Telephone {
+	//set owner's address to public
+  address public owner;
+
+  constructor() {
+		//set owner to the contruct deployer
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    //Authorization check
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
+}
